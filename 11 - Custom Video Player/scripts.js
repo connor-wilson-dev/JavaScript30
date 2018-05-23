b9a56f80d4e91a0e8d4be9c0a47ef17d0adf1776
Connor Wilson <ConnorWilson@pm.me>
2018-05-22 21:05:37 -0400

    Added comments for later viewing implemented the skip funciton from the video, important note, always remember if you are giving a string to something that is expecting a float, add parseFloat() to the value

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,22 +6,27 @@ const progFilled = prog.children[0];
 const vol = player.querySelector('input[name="volume"]');
 const playerControls = player.querySelector('.player__controls');
 const playbackRate = player.querySelector('input[name="volume"]');
-const replayBtn = player.querySelector('button[data-skip="-10"]');
-const skipBtn = player.querySelector('button[data-skip="25"]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 
 function togglePlay() {
+  //RESEARCH How does this method method work????
   const method = video.paused ? 'play' : 'pause';
   video[method]();
 }
 
 function changeIcon() {
+  //icon is equal to the a boolean declaring if paused show play, if playing show pause;
   const icon = this.paused ? '►' : '▌▌';
   toggle.textContent = icon;
 }
-
+function skip() {
+  // sets current video time to be equal to current video time + skip data (-10 or +25);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
 toggle.addEventListener('click', togglePlay)
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', changeIcon);
 video.addEventListener('pause', changeIcon);
-prog.addEventListener('click', function(e) {
-})
+
+skipButtons.forEach(button => button.addEventListener('click', skip)
+)
