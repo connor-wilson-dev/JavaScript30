c5152bc4f05c18a704db9bef79a0722428104452
Connor Wilson <ConnorWilson@pm.me>
2018-05-22 20:55:28 -0400

    updated play and change icons functions with newly learned methods of completing them. I like using the ternary functions to decide what an icon should be, very readable, and will use a lot in the future. I will need to do more reasearch on how to use that 'method' method in the future as it doesn't make a lot of sense to me, but I am very interested in it

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,22 +1,27 @@
-const playBtn = document.querySelector('.player__button');
-const video = document.querySelector('video');
-const prog = document.querySelector('.progress')
+const player = document.querySelector('.player');
+const toggle = player.querySelector('.toggle');
+const video = player.querySelector('video');
+const prog = player.querySelector('.progress')
 const progFilled = prog.children[0];
-const vol = document.querySelector('input[name="volume"]');
+const vol = player.querySelector('input[name="volume"]');
+const playerControls = player.querySelector('.player__controls');
+const playbackRate = player.querySelector('input[name="volume"]');
+const replayBtn = player.querySelector('button[data-skip="-10"]');
+const skipBtn = player.querySelector('button[data-skip="25"]');
 
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
 
+function changeIcon() {
+  const icon = this.paused ? '►' : '▌▌';
+  toggle.textContent = icon;
+}
 
-
-
-playBtn.addEventListener('click', function() {
-  if (video.paused) {
-    video.play();
-    playBtn.innerHTML =  '&#10074;&#10074;';
-  } else if (!video.paused) {
-    video.pause();
-    playBtn.innerHTML = '►';
-  }
+toggle.addEventListener('click', togglePlay)
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', changeIcon);
+video.addEventListener('pause', changeIcon);
+prog.addEventListener('click', function(e) {
 })
-
-prog.addEventListener('click', console.log(progFilled))
-t 
